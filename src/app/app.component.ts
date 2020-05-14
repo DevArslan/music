b85896e2ba90f69280e6480eeae0e693673ec1dc
DevArslan <arslan.gareev.2017@gmail.com>
2020-05-14 05:01:40 +0300

    feat: add animation nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'music';
+
+
+  public changeMusic(event) {
+    const currentMusic = event.srcElement
+    const previousMusic = currentMusic.nextSibling
+    const nextMusic = currentMusic.previousSibling
+    previousMusic.nextSibling.nextSibling.className = 'secondCountry'
+    nextMusic.previousSibling.previousSibling.className = 'firstCountry'
+    previousMusic.className = 'selectMusic-previous'
+    currentMusic.className = 'selectMusic-current'
+    nextMusic.className = 'selectMusic-next'
+    previousMusic.nextSibling.className = 'selectMusic-prev-previous'
+    nextMusic.previousSibling.className = 'selectMusic-next-next'
+
+    // currentMusic.parentElement.firstElementChild.className = 'firstCountry'
+    // currentMusic.parentElement.lastElementChild.className = 'secondCountry'
+  }
+
 }
